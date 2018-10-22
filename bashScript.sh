f73c194d45cb5fac0d4c7da3e908575255af6e88
VishalShitole <vgshitol@ncsu.edu>
2018-10-22 01:13:14 -0400

    Updated Bash Script

--- bashScript.sh
+++ bashScript.sh
@@ -10,21 +10,21 @@ make clean
 make
 
 #Graph 1
-
-for i in 0 1 2 3 4 5 6 7 8 9 10
-do
-  n=1024*2**${i};
-  assoc=32*2**${i}
-for j in 1 2 4 8 ${assoc}
-do
-  ./sim_cache 32 ${n} ${j} 0 0 0 gcc_trace.txt > graph1${i}${j}.txt
-    variable=$(grep 'h.' graph1${i}${j}.txt | awk '{print $6}');
-    variable2=$(($i+10));
-    variable3=$(($j));
-    echo "$variable3,$variable2,$variable" >> graph1.csv
-  echo "Looping ... number $n $i $j"
-done
-done
+#
+#for i in 0 1 2 3 4 5 6 7 8 9 10
+#do
+#  n=1024*2**${i};
+#  assoc=32*2**${i}
+#for j in 1 2 4 8 ${assoc}
+#do
+#  ./sim_cache 32 ${n} ${j} 0 0 0 gcc_trace.txt > graph1${i}${j}.txt
+#    variable=$(grep 'h.' graph1${i}${j}.txt | awk '{print $6}');
+#    variable2=$(($i+10));
+#    variable3=$(($j));
+#    echo "$variable3,$variable2,$variable" >> graph1.csv
+#  echo "Looping ... number $n $i $j"
+#done
+#done
 
 ## graph 2
 #for i in 0 1 2 3 4 5 6 7 8 9 10
@@ -36,13 +36,14 @@ done
 #  ./sim_cache 32 ${n} ${j} 0 0 0 gcc_trace.txt > graph2${i}${j}.txt
 #    variable=$(grep 'h.' graph2${i}${j}.txt | awk '{print $6}');
 #    variable2=$(($i+10));
-#    echo "$variable2,$variable" >> graph2.csv
+#    variable3=$(($j));
+#    echo "$variable3,$variable2,$variable" >> graph2.csv
 #  echo "Looping ... number $n $i $j"
 #done
 #done
-#
-#
-#Graph 3
+
+
+##Graph 3
 #
 #k=1024*512;
 #for i in 0 1 2 3 4 5 6 7 8
@@ -55,11 +56,12 @@ done
 #  variable=$(grep 'h.' graph3${i}${j}.txt | awk '{print $6}');
 #  variable2=$(grep 'n. L2 miss rate:' graph3${i}${j}.txt | awk '{print $5}');
 #  variable3=$(($i+10));
-#    echo "$variable3,$variable2,$variable" >> graph3.csv
+#  variable4=$(($j));
+#   echo "$variable4,$variable3,$variable2,$variable" >> graph3.csv
 #  echo "Looping ... number $n $i $j"
 #done
 #done
-#
+
 ##Graph 4
 #for i in 0 1 2 3 4 5
 #do
@@ -89,47 +91,49 @@ done
 #    variable=$(grep 'h.' graph5${i}${j}.txt | awk '{print $6}');
 #    variable2=$(grep 'n. L2 miss rate:' graph5${i}${j}.txt | awk '{print $5}');
 #    variable3=$(($i+10));
-#    echo "$variable3,$variable2,$variable" >> graph5.csv
+#    variable4=$(($j));
+#    echo "$variable4,$variable3,$variable2,$variable" >> graph5.csv
 #    echo "Looping ... number $n $i $j"
 #  fi
 #done
 #done
-#
-#
-#
-##Graph 6
-#
-#k=64*1024;
-#
-#for i in 0 1 2 3 4 5
-#do
-#  n=1024*2**${i};
-#
-#for j in 0 2 4 8 16
-#do
-#     ./sim_cache 32 ${n} 1 ${j} ${k} 8 gcc_trace.txt > graph6${i}${j}.txt
-#  variable=$(grep 'h.' graph6${i}${j}.txt | awk '{print $6}');
-#  variable2=$(grep 'n. L2 miss rate:' graph6${i}${j}.txt | awk '{print $5}');
-#  variable4=$(grep 'f. swap request rate:' graph6${i}${j}.txt | awk '{print $5}');
-#  variable3=$(($i+10));
-#    echo "$variable3,$variable2,$variable4,$variable" >> graph6.csv
-#  echo "Looping ... number $n $i $j"
-#
-#done
-#     ./sim_cache 32 ${n} 2 0 ${k} 8 gcc_trace.txt > graph6${i}2.txt
-# variable=$(grep 'h.' graph6${i}2.txt | awk '{print $6}');
-#  variable2=$(grep 'n. L2 miss rate:' graph6${i}2.txt | awk '{print $5}');
-#  variable4=$(grep 'f. swap request rate:' graph6${i}2.txt | awk '{print $5}');
-#  variable3=$(($i+10));
-#    echo "$variable3,$variable2,$variable4,$variable" >> graph6.csv
-#  echo "Looping ... number $n $i $j"
-#
-#     ./sim_cache 32 ${n} 4 0 ${k} 8 gcc_trace.txt > graph6${i}4.txt
-#  variable=$(grep 'h.' graph6${i}4.txt | awk '{print $6}');
-#  variable2=$(grep 'n. L2 miss rate:' graph6${i}4.txt | awk '{print $5}');
-#  variable4=$(grep 'f. swap request rate:' graph6${i}4.txt | awk '{print $5}');
-#  variable3=$(($i+10));
-#    echo "$variable3,$variable2,$variable4,$variable" >> graph6.csv
-#  echo "Looping ... number $n $i $j"
-#
-#done
+
+
+
+#Graph 6
+
+k=64*1024;
+
+for i in 0 1 2 3 4 5
+do
+  n=1024*2**${i};
+
+for j in 0 2 4 8 16
+do
+     ./sim_cache 32 ${n} 1 ${j} ${k} 8 gcc_trace.txt > graph6${i}${j}.txt
+  variable=$(grep 'h.' graph6${i}${j}.txt | awk '{print $6}');
+  variable2=$(grep 'n. L2 miss rate:' graph6${i}${j}.txt | awk '{print $5}');
+  variable4=$(grep 'f. swap request rate:' graph6${i}${j}.txt | awk '{print $5}');
+  variable3=$(($i+10));
+  variable5=$(($j));
+    echo "1,$variable5,$variable3,$variable2,$variable4,$variable" >> graph6.csv
+  echo "Looping ... number $n $i $j"
+
+done
+     ./sim_cache 32 ${n} 2 0 ${k} 8 gcc_trace.txt > graph6${i}2.txt
+ variable=$(grep 'h.' graph6${i}2.txt | awk '{print $6}');
+  variable2=$(grep 'n. L2 miss rate:' graph6${i}2.txt | awk '{print $5}');
+  variable4=$(grep 'f. swap request rate:' graph6${i}2.txt | awk '{print $5}');
+  variable3=$(($i+10));
+    echo "2,0,$variable3,$variable2,$variable4,$variable" >> graph6.csv
+  echo "Looping ... number $n $i $j"
+
+     ./sim_cache 32 ${n} 4 0 ${k} 8 gcc_trace.txt > graph6${i}4.txt
+  variable=$(grep 'h.' graph6${i}4.txt | awk '{print $6}');
+  variable2=$(grep 'n. L2 miss rate:' graph6${i}4.txt | awk '{print $5}');
+  variable4=$(grep 'f. swap request rate:' graph6${i}4.txt | awk '{print $5}');
+  variable3=$(($i+10));
+    echo "4,0,$variable3,$variable2,$variable4,$variable" >> graph6.csv
+  echo "Looping ... number $n $i $j"
+
+done
